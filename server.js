33c84f065fafbb2309ffc244c75c5bbafc064166
Richard Dowsett <richarddowsett@gmail.com>
2016-01-22 23:02:39 +0000

    add static files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var port = process.env.PORT || 3000;
 server.listen(port);
-
+app.use(express.static('bower_components'))
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
