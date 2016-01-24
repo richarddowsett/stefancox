9029057935668dd2003fe168c3465b3c399cf6db
Richard Dowsett <richarddowsett@macbook-pro.home>
2016-01-24 12:51:17 -0800

    add img dir

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ server.listen(port);
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/css',  express.static(__dirname + '/css'));
-app.use('img', express.static(__dirname + '/img'));
+app.use('/img', express.static(__dirname + '/img'));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
